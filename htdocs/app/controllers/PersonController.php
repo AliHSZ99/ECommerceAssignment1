296ffdeff1e49dae1ad1bb51fe5e7b174577e007
vincent <you@example.com>
2021-09-18 13:25:39 -0400

    Worked on person controller and index view

--- htdocs/app/controllers/PersonController.php
+++ htdocs/app/controllers/PersonController.php
@@ -5,8 +5,12 @@ namespace app\controllers;
 class PersonController extends \app\core\Controller {
 
     public function index() {
+        $myPerson = new \app\models\Person(); //Create this object to access Person methods
 
-        
+        $data = $myPerson->getAllPeople(); //Use to retrieve all the data from the database 
 
+        $this->view("/Main/index", $data);
     }
+
+    
 }
